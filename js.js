fc53304dcab7594481d211dc3a07d5a519f2d143
VladokAS <vladok.ru139@gmail.com>
2017-07-27 14:22:32 +0300

    Animated scrolling

--- js.js
+++ js.js
@@ -1,7 +1,30 @@
 /**
  * Created by vlado on 25.07.2017.
  */
+
 $("document").ready(function () {
     $("#main_well").fadeIn();
+
+    $(window).scroll(function () {
+        var top = $(window).scrollTop() / 1.3;
+        $("#anim").css("transform", "rotate(" + top + "deg)");
+        $("#anim").css({"margin-top": ($(window).scrollTop()) / 1.9 + "px"});
+
+    });
+
 });
 
+$(window).one(function () {
+    var win_bottom = $(window).scrollTop() + $(window).innerHeight();
+    var $element = $('#timeline');
+
+    if (($(window).scrollTop() < $element.offset().top) &&
+        (win_bottom > $element.offset().top)) {
+        console.log("fadeIn");
+
+        $(function () {
+            $element.hide().fadeIn(1000);
+        });
+    }
+    ;
+});
